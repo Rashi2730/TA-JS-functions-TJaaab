e253bf6674872f2193a93385a9aeef905a6bee96
Rashi Israni <rashiisrani97@gmail.com>
2022-01-05 08:47:18 +0530

    ASS&&

--- block-BJaabw/code/index.js
+++ block-BJaabw/code/index.js
@@ -11,10 +11,15 @@ minToSec(2) ➞ 120
 */
 
 // - Write a Function Decleration for above problem
-function minToSec() {
+function minToSec(minutes, conversion = 60) {
   // Your code
+  let totalSeconds = minutes * conversion ;
+  return totalSeconds ;
 }
 // - Execute the function with required parameter
+minToSec(50);
+minToSec(13); 
+minToSec(2);
 
 /* 2. 
 Create a function named isInRange which validates whether a number n is exclusively within the bounds of lower and upper.
@@ -26,10 +31,14 @@ isInRange(1, 10, 19); // false
 */
 
 // - Write a Function Decleration for above problem
-function isInRange() {
+function isInRange(lower, upper, num) {
   // Your code
+  if(num>=lower && num <= upper) return true;
+  else return false;
 }
 // - Execute the function with required parameter
+isInRange(1, 20, 9); // true
+isInRange(1, 10, 19); // false
 
 /* 2. calculateBMI
 
@@ -49,9 +58,17 @@ Obese: BMI is 30 or more
 
 */
 
-function calculateBMI() {
+function calculateBMI(weight, height) {
   // Your code
+  let BMI = weight / (height* height);
+  if(BMI < 18.5) return "Underweight";
+  else if(BMI >= 18.5 && BMI <= 24.9)  return "Normal weight" ;
+  else if (BMI >= 25 && BMI <= 29.9)   return "Overweight" ;
+  else if (BMI >= 30)  return "Obese" ;
 }
+
+calculateBMI(32, 12);
+
 
 /* 3. appropiateDrinks
 
@@ -64,8 +81,12 @@ Create a function that take the age are return the appropiate drink based on the
 
 */
 
-function appropiateDrinks() {
+function appropiateDrinks(age) {
   // Your code
+  if(age < 14) return "drink fruit juice";
+  else if (age === 18) return "drink soda";
+  else if (age >18 && age < 21)  return "drink fruit-flavored beer";
+  else if (age >= 21) return "drink throat-piercing vodka"
 }
 
 /* 4. Add two numers or string
@@ -79,8 +100,15 @@ Twist is when user passes anything other than number, or string value you should
 
 */
 
-function sum() {
+function sum(numA, numB) {
   // Your code
+  if(typeof numA && typeof numB  ===  ){
+    let sumOfNum = 0 ;
+    sumOfNum = numA + numB 
+    
+    
+
+  }
 }
 
 // Function Test
